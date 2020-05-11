8df790a57b66ae8f788c1d90accb5b4b77d3f0fb
Daniel Turkington <turkington.ad@gmail.com>
2020-05-11 22:20:50 +0100

    Built in word checking functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,4 @@
 require 'open-uri'
-require 'json'
 
 class GamesController < ApplicationController
   def new
@@ -8,29 +7,19 @@ class GamesController < ApplicationController
   end
 
   def score
-    @answer = params[:answer]
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
   def included?(guess, grid)
     guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
   end
 
-  def score_and_message(attempt, grid, time)
-    if included?(attempt.upcase, grid)
-      if english_word?(attempt)
-        score = compute_score(attempt, time)
-        [score, "well done"]
-      else
-        [0, "not an english word"]
-      end
-    else
-      [0, "not in the grid"]
-    end
-  end
-
   def english_word?(word)
     response = open("https://wagon-dictionary.herokuapp.com/#{word}")
     json = JSON.parse(response.read)
-    return json['found']
+    json['found']
   end
 end
