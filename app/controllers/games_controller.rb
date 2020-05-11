a5740bed723df2913d3abf7191ff25a2df0325f0
Daniel Turkington <turkington.ad@gmail.com>
2020-05-11 16:31:42 +0100

    Base of third challenge

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,36 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    letter_array = ('A'..'Z').to_a
+    @letters = letter_array.sample(10)
+  end
+
+  def score
+    @answer = params[:answer]
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def score_and_message(attempt, grid, time)
+    if included?(attempt.upcase, grid)
+      if english_word?(attempt)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
